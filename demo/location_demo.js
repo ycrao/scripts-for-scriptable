9b248d0518987d87f33704fa8ed89a09ec385400
raoyc <raoyc@foxmail.com>
2023-01-09 17:38:10 +0800

    add location demo

--- demo/location_demo.js
+++ demo/location_demo.js
@@ -1,2 +1,12 @@
 // location_demo.js
 // only run in `Scriptable` app
+const location = await Location.current()
+// {"latitude":0,"horizontalAccuracy":0,"longitude":0,"verticalAccuracy":0,"altitude":0}
+console.log(location)
+let latitude = location.latitude
+let longitude = location.longitude
+// see https://sunrise-sunset.org/api
+let url = 'https://api.sunrise-sunset.org/json?lat=' + latitude + '&lng=' + longitude + '&formatted=0&date=today'
+sunDataRaw = await new Request(url).loadJSON()
+
+console.log(sunDataRaw)
