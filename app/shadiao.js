6c2de6b2a9f85cf783cbceefd5600c0407b846ad
raoyc <raoyc@foxmail.com>
2023-01-31 17:29:02 +0800

    add shadiao hot news

---
+++ app/shadiao.js
@@ -0,0 +1,6 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// always-run-in-app: true; icon-color: teal;
+// icon-glyph: crow;
+let url = "https://shadiao.pro/hotnews"
+Safari.openInApp(url)
