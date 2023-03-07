333c589357e6fc5c094d4f850ffd0b16e4ee3220
raoyc <raoyc@foxmail.com>
2023-03-07 11:57:14 +0800

    using new api

--- app/videoHelper.js
+++ app/videoHelper.js
@@ -12,8 +12,39 @@ repo_file_url: https://github.com/ycrao/scripts-for-scriptable/blob/main/app/vid
 raw_file_url: https://raw.githubusercontent.com/ycrao/scripts-for-scriptable/main/app/videoHelper.js
 */
 const $http = importModule("http.module")
-// 抖音/快手/火山/微博/虎牙/轻视频/梨视频/皮皮虾/皮皮搞笑/微视/最右/VUE Vlog/新片场/度小视/六间房/acfun
+
+// 最新可用接口
 async function fetchVideo(clip) {
+  const re = /(http|https):\/\/([a-zA-Z0-9.:?=&-/%#]+)/g
+  const result = clip.match(re)
+  console.log(result)
+  if (result != null && result[0] !== undefined) {
+    const dyUrl = result[0]
+    const apiUrl = "https://api.iculture.cc/api/video/"
+    json = await $http.req("get", apiUrl, {url: dyUrl}).loadJSON()
+    console.log(json)
+    if (json.code === 200) {
+      let videoUrl = json.url
+      if (videoUrl.startsWith("//")) {
+        videoUrl = "https:" + videoUrl
+      }
+      const cb = new CallbackURL("shortcuts://x-callback-url/run-shortcut")
+      cb.addParameter("name", "下载文件")
+      cb.addParameter("input", "text")
+      cb.addParameter("text", videoUrl)
+      cb.open()
+      notify("VideoHelper", "Please select file path to download/请选择路径下载")
+      // Safari.open(videoUrl)
+      return
+    }
+  } else {
+    notify("VideoHelper", "Invalid URL/无效URL")
+  }
+}
+
+// 该接口已失效 抖音/快手/火山/微博/虎牙/轻视频/梨视频/皮皮虾/皮皮搞笑/微视/最右/VUE Vlog/新片场/度小视/六间房/acfun
+/*
+async function _fetchVideo(clip) {
     const re = /(http|https):\/\/([a-zA-Z0-9.:?=&-/%#]+)/g
     const result = clip.match(re)
     console.log(result)
@@ -40,7 +71,7 @@ async function fetchVideo(clip) {
       notify("VideoHelper", "Invalid URL/无效URL")
     }
 }
-
+*/
 
 async function notify(title, body, url) {
   let notification = new Notification()
